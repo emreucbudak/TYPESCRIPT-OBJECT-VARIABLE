908924ca8ac987b6314550c347ac793925d9cdac
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-09-05 15:30:47 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var obj;
+obj = {
+    id: 16,
+    ad: "Emre Üçbudak",
+    meslek: "Software Engineer"
+};
+console.log(obj.id);
---
+++ app.ts
@@ -0,0 +1,14 @@
+type Person = {
+    id: number,
+    ad: string,
+    meslek: string
+}
+let obj: Person;
+obj = {
+
+    id: 16,
+    ad: "Emre Üçbudak",
+    meslek: "Software Engineer"
+
+}
+console.log(obj.id)
